c58355ae17f896625558501f20a2e38fdce2146f
Baptiste Busch <baptiste.busch@gmail.com>
2021-02-02 10:55:34 +0100

    fix: install pinocchio from precompiled packages

--- source/robot_model/install.sh
+++ source/robot_model/install.sh
@@ -1,42 +1,28 @@
 #!/bin/sh
-SOURCE_PATH=${PWD}
+SCRIPT=$(readlink -f "$BASH_SOURCE")
+SOURCE_PATH=$(dirname "$SCRIPT")
 
 apt-get update && apt-get install -y \
     libeigen3-dev \
-    zlib1g-dev \
-    libssl-dev \
-    libboost-all-dev \
-    libboost-dev \
-    libboost-filesystem-dev \
-    libboost-system-dev \
-    libboost-chrono-dev \
-    libboost-date-time-dev \
-    libboost-thread-dev \
-    libboost-test-dev \
-    libbz2-dev \
-    libncurses-dev \
-    pax \
-    tar \
-    liburdfdom-dev \
-    libassimp-dev \
-    assimp-utils \
-    doxygen \
-    texlive-latex-extra
+    lsb-release \
+    gnupg2 \
+    curl
 
 # install googletest
 mkdir ${SOURCE_PATH}/lib
 cd ${SOURCE_PATH}/lib && git clone https://github.com/google/googletest.git
 
 # install pinocchio
-cd ${SOURCE_PATH}/lib && git clone git://git.openrobots.org/robots/robotpkg
-cd ${SOURCE_PATH}/lib/robotpkg/bootstrap && ./bootstrap --prefix=${SOURCE_PATH}/lib/openrobots
-export ROBOTPKG_BASE="${SOURCE_PATH}/lib/openrobots"
-cd ${SOURCE_PATH}/lib/robotpkg/math/pinocchio && make update
+echo "deb [arch=amd64] http://robotpkg.openrobots.org/packages/debian/pub $(lsb_release -cs) robotpkg" | tee /etc/apt/sources.list.d/robotpkg.list
+curl http://robotpkg.openrobots.org/packages/debian/robotpkg.key | apt-key add -
 
-export PATH="${SOURCE_PATH}/lib/openrobots/bin:${PATH}"
-export PKG_CONFIG_PATH="${SOURCE_PATH}/lib/openrobots/lib/pkgconfig:${PKG_CONFIG_PATH}"
-export LD_LIBRARY_PATH="${SOURCE_PATH}/lib/openrobots/lib:${LD_LIBRARY_PATH}"
-export PYTHONPATH="${SOURCE_PATH}/lib/openrobots/lib/python3.6/site-packages:${PYTHONPATH}"
+apt-get update && apt-get install -y robotpkg-py38-pinocchio
+
+export PATH=/opt/openrobots/bin:$PATH
+export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/opt/openrobots/lib/python3.8/site-packages:$PYTHONPATH # Adapt your desired python version here
+export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
 
 # install osqp
 cd ${SOURCE_PATH}/lib && git clone --recursive https://github.com/oxfordcontrol/osqp
@@ -47,3 +33,6 @@ cd ${SOURCE_PATH}/lib/osqp-eigen && mkdir build && cd build && cmake .. && make 
 
 # install robot_model
 cd ${SOURCE_PATH} && mkdir -p build && cd build && cmake -Druntests="OFF" .. && make -j && make install
+
+# reset location
+cd ${SOURCE_PATH}

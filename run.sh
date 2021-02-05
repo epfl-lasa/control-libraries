b58ab33540e118f506c5ee7f17e8b662d4f82b4f
Enrico Eberhard <32450951+eeberhard@users.noreply.github.com>
2021-02-05 17:56:59 +0100

    Remove slash from volume name in toplevel run (#11)
    * Create variable for VOLUME_NAME that strips the `/` from
    the docker image name. Previously a slash would cause a
    failure as an invalid volume name.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MULTISTAGE_TARGET="development"
 
-NAME=$(echo "${PWD##*/}" | tr _ -)/$MULTISTAGE_TARGET
+NAME=$(echo "${PWD##*/}" | tr _ -)/"${MULTISTAGE_TARGET}"
 TAG=$(echo "$1" | tr _/ -)
 
 ISISOLATED=true # change to  false to use host network
@@ -16,12 +16,14 @@ if [ -z "$TAG" ]; then
     TAG="latest"
 fi
 
+VOLUME_NAME=$(echo "${NAME}_lib_vol" | tr / -)
+
 #create a shared volume to store the lib folder
 docker volume create --driver local \
     --opt type="none" \
     --opt device="${PWD}/source/" \
     --opt o="bind" \
-    "${NAME}_lib_vol"
+    "${VOLUME_NAME}"
 
 xhost +
 docker run \
@@ -29,5 +31,5 @@ docker run \
     --net="${NETWORK}" \
     -it \
     --rm \
-    --volume="${NAME}_lib_vol:/home/udev/control_lib/:rw" \
+    --volume="${VOLUME_NAME}:/home/udev/control_lib/:rw" \
     "${NAME}:${TAG}"

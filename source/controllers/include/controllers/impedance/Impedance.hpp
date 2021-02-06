ceb14ab7ed07162ad840b90d01c3dee4aa1221f4
Baptiste Busch <baptiste.busch@gmail.com>
2021-02-06 21:38:14 +0100

    feat: add a generic impedance controller
    The impedance controller is designed to work similarly in task and joint
    spaces. It returns a wrench (or torque) command from a state error

---
+++ source/controllers/include/controllers/impedance/Impedance.hpp
@@ -0,0 +1,133 @@
+/**
+ * @author Baptiste Busch
+ * @date 2021/02
+ *
+ */
+
+#pragma once
+
+#include "controllers/Controller.hpp"
+#include "state_representation/Parameters/Parameter.hpp"
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+
+namespace controllers {
+namespace impedance {
+/**
+ * @class Impedance
+ * @brief Definition of an impedance controller in either joint or task space
+ * @tparam S the space of the controller either CartesianState or JointState
+ */
+template <class S>
+class Impedance : Controller<S, S> {
+private:
+  std::shared_ptr<StateRepresentation::Parameter<Eigen::MatrixXd>> stiffness_;///< stiffness matrix of the controller associated to position
+  std::shared_ptr<StateRepresentation::Parameter<Eigen::MatrixXd>> damping_;  ///< damping matrix of the controller associated to velocity
+  std::shared_ptr<StateRepresentation::Parameter<Eigen::MatrixXd>> inertia_;  ///< inertia matrix of the controller associated to acceleration
+
+public:
+  /**
+   * @brief Constructor initializing all the matrices
+   * @param stiffness the stiffness matrix
+   * @param damping the damping matrix
+   * @param inertia the inertia matrix
+   */
+  explicit Impedance(const Eigen::MatrixXd& stiffness, const Eigen::MatrixXd& damping, const Eigen::MatrixXd& inertia);
+
+  /**
+   * @brief Compute the force (task space) or torque (joint space) command based on the input state 
+   * of the system as the error between the desired state and the real state.
+   * @param desired_state the desired state to reach
+   * @param feedback_state the real state of the system as read from feedback loop
+   * @return the output command at the input state
+   */
+  virtual const S compute_command(const S& desired_state, const S& feedback_state) const;
+
+  /**
+   * @brief Getter of the stiffness matrix
+   * @return the stiffness matrix
+   */
+  const Eigen::MatrixXd& get_stiffness() const;
+
+  /**
+   * @brief Getter of the dmaping matrix
+   * @return the damping matrix
+   */
+  const Eigen::MatrixXd& get_damping() const;
+
+  /**
+   * @brief Getter of the inertia matrix
+   * @return the inertia matrix
+   */
+  const Eigen::MatrixXd& get_inertia() const;
+
+  /**
+   * @brief Setter of the stiffness matrix
+   * @param the new stiffness matrix
+   */
+  void set_stiffness(const Eigen::MatrixXd& stiffness);
+
+  /**
+   * @brief Setter of the damping matrix
+   * @param the new damping matrix
+   */
+  void set_damping(const Eigen::MatrixXd& damping);
+
+  /**
+   * @brief Setter of the inertia matrix
+   * @param the new inertia matrix
+   */
+  void set_inertia(const Eigen::MatrixXd& inertia);
+
+  /**
+   * @brief Return a list of all the parameters of the controller
+   * @return the list of parameters
+   */
+  virtual const std::list<std::shared_ptr<StateRepresentation::ParameterInterface>> get_parameters() const;
+};
+
+template <class S>
+Impedance<S>::Impedance(const Eigen::MatrixXd& stiffness, const Eigen::MatrixXd& damping, const Eigen::MatrixXd& inertia) : stiffness_(std::make_shared<StateRepresentation::Parameter<Eigen::MatrixXd>>("stiffness", stiffness)),
+                                                                                                                            damping_(std::make_shared<StateRepresentation::Parameter<Eigen::MatrixXd>>("damping", damping)),
+                                                                                                                            inertia_(std::make_shared<StateRepresentation::Parameter<Eigen::MatrixXd>>("inertia", inertia)) {}
+
+template <class S>
+inline const Eigen::MatrixXd& Impedance<S>::get_stiffness() const {
+  return this->stiffness_->get_value();
+}
+
+template <class S>
+inline const Eigen::MatrixXd& Impedance<S>::get_damping() const {
+  return this->damping_->get_value();
+}
+
+template <class S>
+inline const Eigen::MatrixXd& Impedance<S>::get_inertia() const {
+  return this->inertia_->get_value();
+}
+
+template <class S>
+inline void Impedance<S>::set_stiffness(const Eigen::MatrixXd& stiffness) {
+  this->stiffness_->set_value(stiffness);
+}
+
+template <class S>
+inline void Impedance<S>::set_damping(const Eigen::MatrixXd& damping) {
+  this->damping_->set_value(damping);
+}
+
+template <class S>
+inline void Impedance<S>::set_inertia(const Eigen::MatrixXd& inertia) {
+  this->inertia_->set_value(inertia);
+}
+
+template <class S>
+const std::list<std::shared_ptr<StateRepresentation::ParameterInterface>> Impedance<S>::get_parameters() const {
+  std::list<std::shared_ptr<StateRepresentation::ParameterInterface>> param_list;
+  param_list.push_back(this->stiffness_);
+  param_list.push_back(this->damping_);
+  param_list.push_back(this->inertia_);
+  return param_list;
+}
+}// namespace impedance
+}// namespace controllers
---
+++ source/controllers/src/impedance/Impedance.cpp
@@ -0,0 +1,22 @@
+#include "controllers/impedance/Impedance.hpp"
+#include "state_representation/Robot/JointState.hpp"
+#include "state_representation/Robot/JointTorques.hpp"
+#include "state_representation/Space/Cartesian/CartesianState.hpp"
+#include "state_representation/Space/Cartesian/CartesianWrench.hpp"
+
+namespace controllers {
+namespace impedance {
+template <>
+const StateRepresentation::CartesianState Impedance<StateRepresentation::CartesianState>::compute_command(const StateRepresentation::CartesianState& desired_state,
+                                                                                                          const StateRepresentation::CartesianState& feedback_state) const {
+  StateRepresentation::CartesianState state_error = desired_state - feedback_state;
+  // compute the wrench using the forlmula W = K * e_pos + D * e_vel + I * acc_desired
+  StateRepresentation::CartesianWrench command;
+  // compute force
+  command.set_force(this->get_stiffness() * state_error.get_position() + this->get_damping() * state_error.get_linear_velocity() + this->get_inertia() * desired_state.get_linear_acceleration());
+  // compute torque (orientation requires special care)
+  command.set_torque(this->get_stiffness() * state_error.get_orientation().vec() + this->get_damping() * state_error.get_linear_velocity() + this->get_inertia() * desired_state.get_linear_acceleration());
+  return command;
+}
+}// namespace impedance
+}// namespace controllers

0c70d828cce2a9738cb571d1ebf64e8a79412521
domire8 <dominic.reber@epfl.ch>
2021-02-08 18:49:56 +0100

    Unit tests for robot_model

--- source/robot_model/tests/testRobotModel.cpp
+++ source/robot_model/tests/testRobotModel.cpp
@@ -1,7 +1,88 @@
 #include "robot_model/Model.hpp"
+
 #include <gtest/gtest.h>
 
+#include "robot_model/Exceptions/InvalidJointStateSizeException.hpp"
+#include "robot_model/Exceptions/FrameNotFoundException.hpp"
+
 using namespace RobotModel;
+
+class RobotModelTest : public testing::Test {
+protected:
+  void SetUp() override {
+    robot_name = "franka";
+    urdf = "/tmp/panda_arm.urdf";
+    franka = Model(robot_name, urdf);
+    joint_state = StateRepresentation::JointState(robot_name, 7);
+  }
+
+  Model empty = Model();
+  Model franka;
+  std::string robot_name;
+  std::string urdf;
+
+  StateRepresentation::JointState joint_state;
+};
+
+// test setters and getters
+TEST_F(RobotModelTest, TestName) {
+  empty.set_robot_name(robot_name);
+  EXPECT_EQ(empty.get_robot_name(), robot_name);
+}
+
+TEST_F(RobotModelTest, TestUrdfPath) {
+  empty.set_urdf_path(urdf);
+  EXPECT_EQ(empty.get_urdf_path(), urdf);
+}
+
+TEST_F(RobotModelTest, TestConstructor) {
+  Model tmp(robot_name, urdf);
+  EXPECT_NO_THROW(franka = tmp);
+}
+
+TEST_F(RobotModelTest, TestNbJoint) {
+  EXPECT_EQ(franka.get_nb_joints(), 7);
+}
+
+TEST_F(RobotModelTest, TestForwardGeometryJointStateSize) {
+  StateRepresentation::JointState dummy = StateRepresentation::JointState(robot_name, 6);
+  EXPECT_THROW(franka.forward_geometry(dummy), Exceptions::InvalidJointStateSizeException);
+}
+
+TEST_F(RobotModelTest, TestForwardGeometry) {
+  EXPECT_EQ(franka.forward_geometry(joint_state).get_position(),
+            franka.forward_geometry(joint_state, "panda_link8").get_position());
+}
+
+TEST_F(RobotModelTest, TestForwardGeometryInvalidFrameName) {
+  EXPECT_THROW(franka.forward_geometry(joint_state, "panda_link99"), Exceptions::FrameNotFoundException);
+}
+
+// this fails, we should make forward geometry with frame ids either private or test for frame id existence
+//TEST_F(RobotModelTest, TestForwardGeometryInvalidFrameID) {
+//  std::vector<unsigned int> frame_ids;
+//  frame_ids.push_back(99);
+//  EXPECT_THROW(franka.forward_geometry(joint_state, frame_ids), RobotModel::Exceptions::FrameNotFoundException);
+//}
+
+TEST_F(RobotModelTest, TestJacobianJointStateSize) {
+  StateRepresentation::JointState dummy = StateRepresentation::JointState(robot_name, 6);
+  EXPECT_THROW(franka.compute_jacobian(dummy, 99), Exceptions::InvalidJointStateSizeException);
+}
+
+TEST_F(RobotModelTest, TestJacobianInvalidFrameName) {
+  EXPECT_THROW(franka.compute_jacobian(joint_state, "panda_link99"), Exceptions::FrameNotFoundException);
+}
+
+TEST_F(RobotModelTest, TestJacobianNbRows) {
+  StateRepresentation::Jacobian jac = franka.compute_jacobian(joint_state, 2);
+  EXPECT_EQ(jac.get_nb_rows(),6);
+}
+
+TEST_F(RobotModelTest, TestJacobianNbCols) {
+  StateRepresentation::Jacobian jac = franka.compute_jacobian(joint_state, 2);
+  EXPECT_EQ(jac.get_nb_cols(), joint_state.get_size());
+}
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);

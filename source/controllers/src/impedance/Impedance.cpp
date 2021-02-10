da5ae5402b38fcf419a231ad7c36001ceb58d34e
Baptiste Busch <baptiste.busch@gmail.com>
2021-02-10 14:28:06 +0100

    feat: add the joint state equivalent of the impedance controller

--- source/controllers/src/impedance/Impedance.cpp
+++ source/controllers/src/impedance/Impedance.cpp
@@ -11,11 +11,22 @@ const StateRepresentation::CartesianState Impedance<StateRepresentation::Cartesi
                                                                                                           const StateRepresentation::CartesianState& feedback_state) const {
   StateRepresentation::CartesianState state_error = desired_state - feedback_state;
   // compute the wrench using the forlmula W = K * e_pos + D * e_vel + I * acc_desired
-  StateRepresentation::CartesianWrench command;
+  StateRepresentation::CartesianWrench command(feedback_state.get_name(), feedback_state.get_reference_frame());
   // compute force
   command.set_force(this->get_stiffness() * state_error.get_position() + this->get_damping() * state_error.get_linear_velocity() + this->get_inertia() * desired_state.get_linear_acceleration());
   // compute torque (orientation requires special care)
   command.set_torque(this->get_stiffness() * state_error.get_orientation().vec() + this->get_damping() * state_error.get_angular_velocity() + this->get_inertia() * desired_state.get_angular_acceleration());
+  return command;
+}
+
+template <>
+const StateRepresentation::JointState Impedance<StateRepresentation::JointState>::compute_command(const StateRepresentation::JointState& desired_state,
+                                                                                                  const StateRepresentation::JointState& feedback_state) const {
+  StateRepresentation::JointState state_error = desired_state - feedback_state;
+  // compute the wrench using the forlmula W = K * e_pos + D * e_vel + I * acc_desired
+  StateRepresentation::JointTorques command(feedback_state.get_name(), feedback_state.get_names());
+  // compute torques
+  command.set_torques(this->get_stiffness() * state_error.get_positions() + this->get_damping() * state_error.get_velocities() + this->get_inertia() * desired_state.get_accelerations());
   return command;
 }
 }// namespace impedance

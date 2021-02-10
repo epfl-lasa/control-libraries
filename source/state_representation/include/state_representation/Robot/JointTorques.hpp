18f9565b22a8928b49f7e35bcb2f2acdcd1ef381
Baptiste Busch <baptiste.busch@gmail.com>
2021-02-10 10:44:02 +0100

    fix: correct for now the clamp functions with a double

--- source/state_representation/include/state_representation/Robot/JointTorques.hpp
+++ source/state_representation/include/state_representation/Robot/JointTorques.hpp
@@ -163,11 +163,28 @@ public:
 
   /**
    * @brief Clamp inplace the magnitude of the velocity to the values in argument
+   * @param max_absolute the maximum magnitude of torque for all the joints
+   * @param noise_ratio if provided, this value will be used to apply a deadzone under which
+   * the torque will be set to 0
+   */
+  void clamp(double max_absolute, double noise_ratio);
+
+  /**
+   * @brief Clamp inplace the magnitude of the velocity to the values in argument
+   * @param max_absolute the maximum magnitude of torque for all the joints
+   * @param noise_ratio if provided, this value will be used to apply a deadzone under which
+   * the torque will be set to 0
+   * @return the clamped JointTorques
+   */
+  JointTorques clamped(double max_absolute, double noise_ratio) const;
+
+  /**
+   * @brief Clamp inplace the magnitude of the velocity to the values in argument
    * @param max_absolute the maximum magnitude of torque for each joint 
    * @param noise_ratio if provided, this value will be used to apply a deadzone under which
    * the torque will be set to 0
    */
-  void clamp(const Eigen::ArrayXd& max_absolute, const Eigen::ArrayXd& noise_ratio);
+  //void clamp(const Eigen::ArrayXd& max_absolute, const Eigen::ArrayXd& noise_ratio);
 
   /**
    * @brief Clamp inplace the magnitude of the velocity to the values in argument
@@ -176,7 +193,7 @@ public:
    * the torque will be set to 0
    * @return the clamped JointTorques
    */
-  JointTorques clamped(const Eigen::ArrayXd& max_absolute, const Eigen::ArrayXd& noise_ratio) const;
+  //JointTorques clamped(const Eigen::ArrayXd& max_absolute, const Eigen::ArrayXd& noise_ratio) const;
 
   /**
    * @brief Overload the ostream operator for printing

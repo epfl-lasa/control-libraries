b7f1cf2fd8b9d0c6d885948085f203dae6a60859
Baptiste Busch <baptiste.busch@gmail.com>
2021-02-16 11:30:23 +0100

    feat: add the Dissipative Impedance controller (aka PassiveDS)

---
+++ source/controllers/include/controllers/impedance/Dissipative.hpp
@@ -0,0 +1,147 @@
+/**
+ * @author Baptiste Busch
+ * @date 2021/02
+ *
+ */
+
+#pragma once
+
+#include "controllers/impedance/Impedance.hpp"
+#include "state_representation/Parameters/Parameter.hpp"
+#include "state_representation/Space/Cartesian/CartesianState.hpp"
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+
+namespace controllers {
+namespace impedance {
+/**
+ * @enum ComputationalSpaceType
+ * @brief Selector of the space in which the controller should be computed
+ * LINEAR and ANGULAR only compute the command in linear and angular space
+ * respectively, TWIST applies it on the full twist vector, and
+ * DECOUPLED_TWIST (default) computes the damping matrix for the linear
+ * and angular part separatly
+ */
+enum class ComputationalSpaceType {
+  LINEAR,
+  ANGULAR,
+  TWIST,
+  DECOUPLED_TWIST
+};
+
+/**
+ * @class Dissipative
+ * @brief Definition of a dissipative impedance controller (PassiveDS) in task space
+ */
+class Dissipative : Impedance<StateRepresentation::CartesianState> {
+private:
+  ComputationalSpaceType computational_space_;                                          ///< the space in which to compute the command vector
+  Eigen::Matrix<double, 6, 6> basis_;                                                   ///< basis matrix used to compute the damping matrix
+  std::shared_ptr<StateRepresentation::Parameter<Eigen::VectorXd>> damping_eigenvalues_;///< coefficient of eigenvalues used in the damping matrix computation
+
+  /**
+   * @brief Orthonormalize inplace the basis matrix given in argument
+   * @param basis the basis matrix to orthonormalize
+   */
+  static void orthonormalize(Eigen::MatrixXd& basis);
+
+  /**
+   * @brief Compute the orthonormal basis based on the main eigenvector in parameter
+   * @param basis the basis matrix to orthonormalize
+   * @param main_eigenvector the main eigenvector used to compute the basis.
+   * Other eigenvectora are orthogonal and selected randomly
+   */
+  Eigen::MatrixXd compute_orthonormal_basis(const Eigen::MatrixXd& basis, const Eigen::VectorXd& main_eigenvector) const;
+
+  /**
+   * @brief Compute the damping matrix as the orthonormal basis
+   * to the direction of the desired velocity
+   * @param desired_velocity the velocity from which the direction
+   * of motion is extracted to compute the basis
+   */
+  void compute_damping(const Eigen::VectorXd& desired_velocity);
+
+  /**
+   * @brief Get the eigenvalues in a diagonal matrix form
+   */
+  Eigen::DiagonalMatrix<double, 6, 6> get_diagonal_eigenvalues() const;
+
+public:
+  /**
+   * @brief Constructor with specified computational space. Default is DECOUPLED_TWIST
+   */
+  explicit Dissipative(const ComputationalSpaceType& computational_space = ComputationalSpaceType::DECOUPLED_TWIST);
+
+  /**
+   * @brief Getter of the damping eigenvalues parameter
+   * @return the eigenvalues as a Vetor6D
+   */
+  Eigen::Matrix<double, 6, 1> get_damping_eigenvalues() const;
+
+  /**
+   * @brief Getter of the damping eigenvalue corresponding to the specified index
+   * @param index the index of the eigenvalue (number between 0 and 5)
+   * @return the eigenvalue at desired index
+   */
+  double get_damping_eigenvalue(unsigned int index) const;
+
+  /**
+   * @brief Setter of the damping eigenvalues parameter
+   * @param damping_eigenvalues the new values of the eigenvalue as a Vector6D
+   */
+  void set_damping_eigenvalues(const Eigen::Matrix<double, 6, 1>& damping_eigenvalues);
+
+  /**
+   * @brief Setter of the damping eigenvalue corresponding to the specified index
+   * @param damping_eigenvalue the new value of the eigenvalue
+   * @param index the index of the eigenvalue (number between 0 and 5)
+   */
+  void set_damping_eigenvalue(double damping_eigenvalue, unsigned int index);
+
+  /**
+   * @brief Compute the force (task space) or torque (joint space) command based on the input state
+   * of the system as the error between the desired state and the real state.
+   * @param desired_state the desired state to reach
+   * @param feedback_state the real state of the system as read from feedback loop
+   * @return the output command at the input state
+   */
+  StateRepresentation::CartesianState compute_command(const StateRepresentation::CartesianState& desired_state,
+                                                      const StateRepresentation::CartesianState& feedback_state);
+
+  /**
+   * @brief Return a list of all the parameters of the controller
+   * @return the list of parameters
+   */
+  std::list<std::shared_ptr<StateRepresentation::ParameterInterface>> get_parameters() const;
+};
+
+inline Eigen::Matrix<double, 6, 1> Dissipative::get_damping_eigenvalues() const {
+  return this->damping_eigenvalues_->get_value();
+}
+
+inline double Dissipative::get_damping_eigenvalue(unsigned int index) const {
+  Eigen::Matrix<double, 6, 1> eigenvalues = this->get_damping_eigenvalues();
+  return eigenvalues(index);
+}
+
+inline void Dissipative::set_damping_eigenvalues(const Eigen::Matrix<double, 6, 1>& damping_eigenvalues) {
+  this->damping_eigenvalues_->set_value(damping_eigenvalues);
+}
+
+inline void Dissipative::set_damping_eigenvalue(double damping_eigenvalue, unsigned int index) {
+  Eigen::Matrix<double, 6, 1> eigenvalues = this->get_damping_eigenvalues();
+  eigenvalues(index) = damping_eigenvalue;
+  this->set_damping_eigenvalues(eigenvalues);
+}
+
+inline Eigen::DiagonalMatrix<double, 6, 6> Dissipative::get_diagonal_eigenvalues() const {
+  return this->get_damping_eigenvalues().asDiagonal();
+}
+
+inline std::list<std::shared_ptr<StateRepresentation::ParameterInterface>> Dissipative::get_parameters() const {
+  std::list<std::shared_ptr<StateRepresentation::ParameterInterface>> param_list;
+  param_list.push_back(this->damping_eigenvalues_);
+  return param_list;
+}
+}// namespace impedance
+}// namespace controllers
---
+++ source/controllers/src/impedance/Dissipative.cpp
@@ -0,0 +1,62 @@
+#include "controllers/impedance/Dissipative.hpp"
+
+namespace controllers {
+namespace impedance {
+Dissipative::Dissipative(const ComputationalSpaceType& computational_space) : Impedance<StateRepresentation::CartesianState>(Eigen::MatrixXd::Identity(6, 6),
+                                                                                                                             Eigen::MatrixXd::Zero(6, 6),
+                                                                                                                             Eigen::MatrixXd::Zero(6, 6)),
+                                                                              computational_space_(computational_space),
+                                                                              basis_(Eigen::MatrixXd::Random(6, 6)),
+                                                                              damping_eigenvalues_(std::make_shared<StateRepresentation::Parameter<Eigen::VectorXd>>("damping_eigenvalues", Eigen::ArrayXd::Ones(6))) {}
+
+void Dissipative::orthonormalize(Eigen::MatrixXd& basis) {
+  basis.col(0).normalize();
+  for (uint i = 1; i < basis.cols(); ++i) {
+    for (uint j = 0; j < i; ++j) {
+      basis.col(i) -= basis.col(j).dot(basis.col(i)) * basis.col(j);
+    }
+    basis.col(i).normalize();
+  }
+}
+
+Eigen::MatrixXd Dissipative::compute_orthonormal_basis(const Eigen::MatrixXd& basis, const Eigen::VectorXd& main_eigenvector) const {
+  Eigen::MatrixXd orthonormal_basis = basis;
+  orthonormal_basis.col(0) = main_eigenvector;
+  Dissipative::orthonormalize(orthonormal_basis);
+  return orthonormal_basis;
+}
+
+void Dissipative::compute_damping(const Eigen::VectorXd& desired_twist) {
+  switch (this->computational_space_) {
+    case ComputationalSpaceType::LINEAR:
+      //return only the linear block
+      this->basis_.block<3, 3>(0, 0) = this->compute_orthonormal_basis(this->basis_.block<3, 3>(0, 0), desired_twist.segment(0, 3));
+      this->basis_.block<3, 3>(3, 3).setZero();
+      break;
+    case ComputationalSpaceType::ANGULAR:
+      // return only the angular block
+      this->basis_.block<3, 3>(0, 0).setZero();
+      this->basis_.block<3, 3>(3, 3) = this->compute_orthonormal_basis(this->basis_.block<3, 3>(3, 3), desired_twist.segment(3, 3));
+      break;
+    case ComputationalSpaceType::TWIST:
+      // return the full damping matrix
+      this->basis_ = this->compute_orthonormal_basis(this->basis_, desired_twist);
+      break;
+    case ComputationalSpaceType::DECOUPLED_TWIST:
+      // compute per block
+      this->basis_.block<3, 3>(0, 0) = this->compute_orthonormal_basis(this->basis_.block<3, 3>(0, 0), desired_twist.segment(0, 3));
+      this->basis_.block<3, 3>(3, 3) = this->compute_orthonormal_basis(this->basis_.block<3, 3>(3, 3), desired_twist.segment(3, 3));
+      break;
+  }
+  this->set_damping(this->basis_ * this->get_diagonal_eigenvalues() * this->basis_.transpose());
+}
+
+StateRepresentation::CartesianState Dissipative::compute_command(const StateRepresentation::CartesianState& desired_state,
+                                                                 const StateRepresentation::CartesianState& feedback_state) {
+  // compute the damping matrix out of the desired_state twist
+  this->compute_damping(desired_state.get_twist());
+  // apply the impedance control law
+  return this->Impedance<StateRepresentation::CartesianState>::compute_command(desired_state, feedback_state);
+}
+}// namespace impedance
+}// namespace controllers

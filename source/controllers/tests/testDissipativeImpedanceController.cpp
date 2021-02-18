a38880011a4a2623950d700b66215cdde0386b31
Baptiste Busch <baptiste.busch@gmail.com>
2021-02-18 17:00:57 +0100

    fix: simplify test writing and separate cases in differrent tests

--- source/controllers/tests/testDissipativeImpedanceController.cpp
+++ source/controllers/tests/testDissipativeImpedanceController.cpp
@@ -1,4 +1,5 @@
 #include "controllers/impedance/Dissipative.hpp"
+#include <numeric>
 #include <gtest/gtest.h>
 
 using namespace controllers::impedance;
@@ -13,6 +14,7 @@ protected:
   }
 
   Dissipative controller_;
+  double tolerance_ = 1e-4;
 };
 
 TEST_F(DissipativeImpedanceControllerTest, TestOrthonormalize) {
@@ -24,22 +26,22 @@ TEST_F(DissipativeImpedanceControllerTest, TestOrthonormalize) {
 
   // first column should the normalized eigenvector
   Eigen::Vector3d err = orthonormal_basis.col(0) - eigenvector.normalized();
-  for (int i = 0; i < 3; ++i) { EXPECT_NEAR(err(i), 0., 10e-4); }
+  for (int i = 0; i < 3; ++i) { EXPECT_NEAR(err(i), 0., tolerance_); }
   // all inner products must be equal to 0
-  EXPECT_NEAR(orthonormal_basis.col(0).dot(orthonormal_basis.col(1)), 0., 10e-4);
-  EXPECT_NEAR(orthonormal_basis.col(1).dot(orthonormal_basis.col(2)), 0., 10e-4);
-  EXPECT_NEAR(orthonormal_basis.col(0).dot(orthonormal_basis.col(2)), 0., 10e-4);
+  EXPECT_NEAR(orthonormal_basis.col(0).dot(orthonormal_basis.col(1)), 0., tolerance_);
+  EXPECT_NEAR(orthonormal_basis.col(1).dot(orthonormal_basis.col(2)), 0., tolerance_);
+  EXPECT_NEAR(orthonormal_basis.col(0).dot(orthonormal_basis.col(2)), 0., tolerance_);
   // all the magnitude should be equal to 1
-  EXPECT_NEAR(orthonormal_basis.col(0).norm(), 1., 10e-4);
-  EXPECT_NEAR(orthonormal_basis.col(1).norm(), 1., 10e-4);
-  EXPECT_NEAR(orthonormal_basis.col(2).norm(), 1., 10e-4);
+  EXPECT_NEAR(orthonormal_basis.col(0).norm(), 1., tolerance_);
+  EXPECT_NEAR(orthonormal_basis.col(1).norm(), 1., tolerance_);
+  EXPECT_NEAR(orthonormal_basis.col(2).norm(), 1., tolerance_);
 }
 
-TEST_F(DissipativeImpedanceControllerTest, TestComputeDamping) {
+TEST_F(DissipativeImpedanceControllerTest, TestComputeDampingLinear) {
   Eigen::MatrixXd damping;
   Eigen::Matrix3d identity3 = Eigen::Matrix3d::Identity();
-  Eigen::MatrixXd identity6 = Eigen::MatrixXd::Identity(6, 6);
   Eigen::VectorXd eigenvector = Eigen::VectorXd::Random(6);
+  Eigen::Matrix3d block;
   // case LINEAR
   set_controller_space(ComputationalSpaceType::LINEAR);
   controller_.compute_damping(eigenvector);
@@ -47,56 +49,73 @@ TEST_F(DissipativeImpedanceControllerTest, TestComputeDamping) {
 
   // linear part is identity
   for (int i = 0; i < 3; ++i) {
-    for (int j = 0; j < 3; ++j) { EXPECT_NEAR(damping(i, j), identity3(i, j), 10e-4); }
+    for (int j = 0; j < 3; ++j) {
+      EXPECT_NEAR(damping(i, j), identity3(i, j), tolerance_);
+    }
   }
   // angular part is 0
-  for (int i = 3; i < 6; ++i) {
-    for (int j = 3; j < 6; ++j) { EXPECT_NEAR(damping(i, j), 0., 10e-4); }
-  }
+  block = damping.block<3, 3>(3, 3);
+  EXPECT_NEAR(std::accumulate(block.data(), block.data() + block.size(), 0.0), 0.0, tolerance_);
   // side blocks are 0
-  for (int i = 3; i < 6; ++i) {
-    for (int j = 0; j < 3; ++j) { EXPECT_NEAR(damping(i, j), 0., 10e-4); }
-  }
-  for (int i = 0; i < 3; ++i) {
-    for (int j = 3; j < 6; ++j) { EXPECT_NEAR(damping(i, j), 0., 10e-4); }
-  }
+  block = damping.block<3, 3>(3, 0);
+  EXPECT_NEAR(std::accumulate(block.data(), block.data() + block.size(), 0.0), 0.0, tolerance_);
+  block = damping.block<3, 3>(0, 3);
+  EXPECT_NEAR(std::accumulate(block.data(), block.data() + block.size(), 0.0), 0.0, tolerance_);
+}
 
-  // case ANGULAR
+TEST_F(DissipativeImpedanceControllerTest, TestComputeDampingAngular) {
+  Eigen::MatrixXd damping;
+  Eigen::Matrix3d identity3 = Eigen::Matrix3d::Identity();
+  Eigen::VectorXd eigenvector = Eigen::VectorXd::Random(6);
+  Eigen::Matrix3d block;
   set_controller_space(ComputationalSpaceType::ANGULAR);
   controller_.compute_damping(eigenvector);
   damping = controller_.get_damping();
   // linear part is 0
-  for (int i = 0; i < 3; ++i) {
-    for (int j = 3; j < 3; ++j) { EXPECT_NEAR(damping(i, j), 0., 10e-4); }
-  }
+  block = damping.block<3, 3>(0, 0);
+  EXPECT_NEAR(std::accumulate(block.data(), block.data() + block.size(), 0.0), 0.0, tolerance_);
   // angular part is non null
   for (int i = 3; i < 6; ++i) {
-    for (int j = 3; j < 6; ++j) { EXPECT_NEAR(damping(i, j), identity3(i - 3, j - 3), 10e-4); }
+    for (int j = 3; j < 6; ++j) {
+      EXPECT_NEAR(damping(i, j), identity3(i - 3, j - 3), tolerance_);
+    }
   }
   // side blocks are 0
-  for (int i = 3; i < 6; ++i) {
-    for (int j = 0; j < 3; ++j) { EXPECT_NEAR(damping(i, j), 0., 10e-4); }
-  }
-  for (int i = 0; i < 3; ++i) {
-    for (int j = 3; j < 6; ++j) { EXPECT_NEAR(damping(i, j), 0., 10e-4); }
-  }
+  block = damping.block<3, 3>(3, 0);
+  EXPECT_NEAR(std::accumulate(block.data(), block.data() + block.size(), 0.0), 0.0, tolerance_);
+  block = damping.block<3, 3>(0, 3);
+  EXPECT_NEAR(std::accumulate(block.data(), block.data() + block.size(), 0.0), 0.0, tolerance_);
+}
 
+TEST_F(DissipativeImpedanceControllerTest, TestComputeDampingDecoupledTwist) {
+  Eigen::MatrixXd damping;
+  Eigen::MatrixXd identity6 = Eigen::MatrixXd::Identity(6, 6);
+  Eigen::VectorXd eigenvector = Eigen::VectorXd::Random(6);
   // case DECOUPLED_TWIST
   set_controller_space(ComputationalSpaceType::DECOUPLED_TWIST);
   controller_.compute_damping(eigenvector);
   damping = controller_.get_damping();
   // identity 6
   for (int i = 0; i < 6; ++i) {
-    for (int j = 0; j < 6; ++j) { EXPECT_NEAR(damping(i, j), identity6(i, j), 10e-4); }
+    for (int j = 0; j < 6; ++j) {
+      EXPECT_NEAR(damping(i, j), identity6(i, j), tolerance_);
+    }
   }
+}
 
+TEST_F(DissipativeImpedanceControllerTest, TestComputeDampingTwist) {
+  Eigen::MatrixXd damping;
+  Eigen::MatrixXd identity6 = Eigen::MatrixXd::Identity(6, 6);
+  Eigen::VectorXd eigenvector = Eigen::VectorXd::Random(6);
   // case TWIST
   set_controller_space(ComputationalSpaceType::TWIST);
   controller_.compute_damping(eigenvector);
   damping = controller_.get_damping();
   // identity 6
   for (int i = 0; i < 6; ++i) {
-    for (int j = 0; j < 6; ++j) { EXPECT_NEAR(damping(i, j), identity6(i, j), 10e-4); }
+    for (int j = 0; j < 6; ++j) {
+      EXPECT_NEAR(damping(i, j), identity6(i, j), tolerance_);
+    }
   }
 }
 

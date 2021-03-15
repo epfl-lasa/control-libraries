409885c90eb319ffb72437351d79a45d9d64a98b
Baptiste Busch <baptiste.busch@gmail.com>
2021-03-15 08:26:05 +0100

    fix remove the division by the inertia

--- source/controllers/src/impedance/Impedance.cpp
+++ source/controllers/src/impedance/Impedance.cpp
@@ -32,19 +32,19 @@ template<>
 CartesianState Impedance<CartesianState>::compute_command(const CartesianState& desired_state,
                                                           const CartesianState& feedback_state) {
   CartesianState state_error = desired_state - feedback_state;
-  // compute the wrench using the formula W = acc_desired + I^-1 [K * e_pose + D * e_twist - e_wrench]
+  // compute the wrench using the formula W = I * acc_desired + K * e_pose + D * e_twist - e_wrench
   CartesianWrench command(feedback_state.get_name(), feedback_state.get_reference_frame());
   // compute force
   Eigen::Vector3d force_control = this->get_stiffness().block<3, 3>(0, 0) * state_error.get_position()
       + this->get_damping().block<3, 3>(0, 0) * state_error.get_linear_velocity();
-  Eigen::Vector3d commanded_force = desired_state.get_linear_acceleration()
-      + this->get_inertia().block<3, 3>(0, 0).ldlt().solve(force_control - state_error.get_force());
+  Eigen::Vector3d commanded_force = this->get_inertia().block<3, 3>(0, 0) * desired_state.get_linear_acceleration()
+      + force_control - state_error.get_force();
   command.set_force(commanded_force);
   // compute torque (orientation requires special care)
   Eigen::Vector3d torque_control = this->get_stiffness().block<3, 3>(3, 3) * state_error.get_orientation().vec()
       + this->get_damping().block<3, 3>(3, 3) * state_error.get_angular_velocity();
-  Eigen::Vector3d commanded_torque = desired_state.get_linear_acceleration()
-      + this->get_inertia().block<3, 3>(3, 3).ldlt().solve(torque_control - state_error.get_torque());
+  Eigen::Vector3d commanded_torque = this->get_inertia().block<3, 3>(3, 3) * desired_state.get_angular_acceleration()
+      + torque_control - state_error.get_torque();
   command.set_torque(commanded_torque);
   return command;
 }
@@ -53,13 +53,13 @@ template<>
 JointState Impedance<JointState>::compute_command(const JointState& desired_state,
                                                   const JointState& feedback_state) {
   JointState state_error = desired_state - feedback_state;
-  // compute the wrench using the formula T = acc_desired + I^-1 [K * e_pos + D * e_vel - e_tor]
+  // compute the wrench using the formula T = I * acc_desired + K * e_pos + D * e_vel - e_tor
   JointTorques command(feedback_state.get_name(), feedback_state.get_names());
   // compute torques
   Eigen::VectorXd torque_control = this->get_stiffness() * state_error.get_positions()
       + this->get_damping() * state_error.get_velocities();
-  Eigen::VectorXd commanded_torque = desired_state.get_accelerations()
-      + this->get_inertia().ldlt().solve(torque_control - state_error.get_torques());
+  Eigen::VectorXd commanded_torque = this->get_inertia() * desired_state.get_accelerations()
+      + torque_control - state_error.get_torques();
   command.set_torques(commanded_torque);
   return command;
 }

271e873ed83b37a53e59b74891f1d5b6cb6e7309
Baptiste Busch <baptiste.busch@gmail.com>
2021-03-16 16:51:48 +0100

    fix: add the force error instead of subtracting it

--- source/controllers/src/impedance/Impedance.cpp
+++ source/controllers/src/impedance/Impedance.cpp
@@ -32,19 +32,19 @@ template<>
 CartesianState Impedance<CartesianState>::compute_command(const CartesianState& desired_state,
                                                           const CartesianState& feedback_state) {
   CartesianState state_error = desired_state - feedback_state;
-  // compute the wrench using the formula W = I * acc_desired + K * e_pose + D * e_twist - e_wrench
+  // compute the wrench using the formula W = I * acc_desired + K * e_pose + D * e_twist + e_wrench
   CartesianWrench command(feedback_state.get_name(), feedback_state.get_reference_frame());
   // compute force
   Eigen::Vector3d position_control = this->get_stiffness().topLeftCorner<3,3>() * state_error.get_position()
       + this->get_damping().topLeftCorner<3,3>() * state_error.get_linear_velocity()
       + this->get_inertia().topLeftCorner<3,3>() * desired_state.get_linear_acceleration();
-  Eigen::Vector3d commanded_force = position_control - state_error.get_force();
+  Eigen::Vector3d commanded_force = position_control + state_error.get_force();
   command.set_force(commanded_force);
   // compute torque (orientation requires special care)
   Eigen::Vector3d orientation_control = this->get_stiffness().bottomRightCorner<3,3>() * state_error.get_orientation().vec()
       + this->get_damping().bottomRightCorner<3,3>() * state_error.get_angular_velocity()
       + this->get_inertia().bottomRightCorner<3,3>() * desired_state.get_angular_acceleration();
-  Eigen::Vector3d commanded_torque = orientation_control - state_error.get_torque();
+  Eigen::Vector3d commanded_torque = orientation_control + state_error.get_torque();
   command.set_torque(commanded_torque);
   return command;
 }
@@ -53,13 +53,13 @@ template<>
 JointState Impedance<JointState>::compute_command(const JointState& desired_state,
                                                   const JointState& feedback_state) {
   JointState state_error = desired_state - feedback_state;
-  // compute the wrench using the formula T = I * acc_desired + K * e_pos + D * e_vel - e_tor
+  // compute the wrench using the formula T = I * acc_desired + K * e_pos + D * e_vel + e_tor
   JointTorques command(feedback_state.get_name(), feedback_state.get_names());
   // compute torques
   Eigen::VectorXd state_control = this->get_stiffness() * state_error.get_positions()
       + this->get_damping() * state_error.get_velocities()
       + this->get_inertia() * desired_state.get_accelerations();
-  Eigen::VectorXd commanded_torques = state_control - state_error.get_torques();
+  Eigen::VectorXd commanded_torques = state_control + state_error.get_torques();
   command.set_torques(commanded_torques);
   return command;
 }

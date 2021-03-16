232efca8a6c712e8680ab8188f23f3ac0b323aaa
Enrico Eberhard <32450951+eeberhard@users.noreply.github.com>
2021-03-16 17:12:17 +0100

    Dynamical Systems Refactor (#51)
    * Refactor and clarify "DS reference frame" logic
    
    * Refactor test structure and add more coverage
    
    * Add proper README for Base and Linear DS
    
    ---
    
    Reimplement DS base frame logic
    
    * Refactor reference_frame_ member and associated set/get functions
    to be named base_frame_ in DynamicalSystem class
    
    * Revise DS::evaluate function to always and only return the result
    in the same reference frame as the input, accepting either:
    * * The base frame of the DS
    * * The _reference frame_ of the base frame of the DS
    
    * Add basic constructor to DynamicalSystem to take a string
    argument, from which it automatically makes an Identity state
    for the base frame
    
    * Add logic to Linear DS set_attractor to assert that it matches
    the DS base frame
    
    * Add logic to Linear DS to override set_base_frame and update
    the attractor accordingly
    
    * Extend test suite for Linear DS to cover setters for attractor
    and base frame, and verify behaviour of stacked/fixed/moving frames
    
    * Revise Circular DS and tests
    
    * Override set_base_frame method to update circle center frame
    
    * Add explicit tests for Circular to cover reference frames
    
    * Minor reformat, use namespace in sourcefile to reduce verbosity
    
    ---
    
    Refactor DS tests
    
    * Add test fixtures to linear and circular DS tests
    
    * Refine test cases:
    * * In circular test, actually check for convergence!
    * * In linear test, add tests for diagonal gains
    and for stacked reference frames
    
    * Refactor test structure to use one top-level test runner
    test_dynamical_system, with test sources grouped by test_
    prefix in the tests subfolder.
    
    * Update the CMakeLists accordingly.
    
    * Rename linear and circular tests to have the test_ prefix
    
    ---
    
    Update DS README
    
    * Add more information of base frame and reference frames
    
    * Add practical example of set_base_frame in a control loop

--- source/dynamical_systems/src/Linear.cpp
+++ source/dynamical_systems/src/Linear.cpp
@@ -1,105 +1,119 @@
 #include "dynamical_systems/Linear.hpp"
 
+using namespace state_representation;
+
 namespace DynamicalSystems {
-template <>
-inline void Linear<state_representation::CartesianState>::set_gain(double iso_gain) {
+
+template<>
+inline void Linear<CartesianState>::set_gain(double iso_gain) {
   this->gain_->set_value(iso_gain * Eigen::MatrixXd::Identity(6, 6));
 }
 
-template <>
-inline void Linear<state_representation::JointState>::set_gain(double iso_gain) {
+template<>
+inline void Linear<JointState>::set_gain(double iso_gain) {
   int nb_joints = this->get_attractor().get_size();
   this->gain_->set_value(iso_gain * Eigen::MatrixXd::Identity(nb_joints, nb_joints));
 }
 
-template <>
-inline void Linear<state_representation::CartesianState>::set_gain(const std::vector<double>& diagonal_coefficients) {
+template<>
+inline void Linear<CartesianState>::set_gain(const std::vector<double>& diagonal_coefficients) {
   if (diagonal_coefficients.size() != 6) {
-    throw Exceptions::IncompatibleSizeException("The provided diagonal coefficients do not correspond to the expected size of 6 elements");
+    throw Exceptions::IncompatibleSizeException(
+        "The provided diagonal coefficients do not correspond to the expected size of 6 elements");
   }
   Eigen::VectorXd diagonal = Eigen::VectorXd::Map(diagonal_coefficients.data(), 6);
   this->gain_->set_value(diagonal.asDiagonal());
 }
 
-template <>
-inline void Linear<state_representation::JointState>::set_gain(const std::vector<double>& diagonal_coefficients) {
+template<>
+inline void Linear<JointState>::set_gain(const std::vector<double>& diagonal_coefficients) {
   size_t nb_joints = this->get_attractor().get_size();
   if (diagonal_coefficients.size() != nb_joints) {
-    throw Exceptions::IncompatibleSizeException("The provided diagonal coefficients do not correspond to the expected size of " + std::to_string(nb_joints) + " elements");
+    throw Exceptions::IncompatibleSizeException(
+        "The provided diagonal coefficients do not correspond to the expected size of "
+            + std::to_string(nb_joints) + " elements");
   }
   Eigen::VectorXd diagonal = Eigen::VectorXd::Map(diagonal_coefficients.data(), nb_joints);
   this->gain_->set_value(diagonal.asDiagonal());
 }
 
-template <>
-inline void Linear<state_representation::CartesianState>::set_gain(const Eigen::MatrixXd& gain_matrix) {
+template<>
+inline void Linear<CartesianState>::set_gain(const Eigen::MatrixXd& gain_matrix) {
   if (gain_matrix.rows() != 6 && gain_matrix.cols() != 6) {
     throw Exceptions::IncompatibleSizeException("The provided gain matrix do not have the expected size of 6x6 elements");
   }
   this->gain_->set_value(gain_matrix);
 }
 
-template <>
-inline void Linear<state_representation::JointState>::set_gain(const Eigen::MatrixXd& gain_matrix) {
+template<>
+inline void Linear<JointState>::set_gain(const Eigen::MatrixXd& gain_matrix) {
   int nb_joints = this->get_attractor().get_size();
   if (gain_matrix.rows() != nb_joints && gain_matrix.cols() != nb_joints) {
-    throw Exceptions::IncompatibleSizeException("The provided gain matrix do not have the expected size of " + std::to_string(nb_joints) + "x" + std::to_string(nb_joints) + " elements");
+    throw Exceptions::IncompatibleSizeException(
+        "The provided gain matrix do not have the expected size of " + std::to_string(nb_joints) + "x"
+            + std::to_string(nb_joints) + " elements");
   }
   this->gain_->set_value(gain_matrix);
 }
 
-template <>
-Linear<state_representation::CartesianState>::Linear(const state_representation::CartesianState& attractor, double iso_gain) : DynamicalSystem<state_representation::CartesianState>(state_representation::CartesianPose::Identity(attractor.get_reference_frame())),
-                                                                                                                               attractor_(std::make_shared<state_representation::Parameter<state_representation::CartesianState>>(state_representation::Parameter<state_representation::CartesianPose>("attractor", attractor))),
-                                                                                                                               gain_(std::make_shared<state_representation::Parameter<Eigen::MatrixXd>>("gain")) {
+template<>
+Linear<CartesianState>::Linear(const CartesianState& attractor, double iso_gain) :
+    DynamicalSystem<CartesianState>(attractor.get_reference_frame()),
+    attractor_(std::make_shared<Parameter<CartesianState>>(Parameter<CartesianPose>("attractor", attractor))),
+    gain_(std::make_shared<Parameter<Eigen::MatrixXd>>("gain")) {
   this->set_gain(iso_gain);
 }
 
-template <>
-Linear<state_representation::JointState>::Linear(const state_representation::JointState& attractor, double iso_gain) : DynamicalSystem<state_representation::JointState>(),
-                                                                                                                       attractor_(std::make_shared<state_representation::Parameter<state_representation::JointState>>(state_representation::Parameter<state_representation::JointPositions>("attractor", attractor))),
-                                                                                                                       gain_(std::make_shared<state_representation::Parameter<Eigen::MatrixXd>>("gain")) {
+template<>
+Linear<JointState>::Linear(const JointState& attractor, double iso_gain) :
+    DynamicalSystem<JointState>(),
+    attractor_(std::make_shared<Parameter<JointState>>(Parameter<JointPositions>("attractor", attractor))),
+    gain_(std::make_shared<Parameter<Eigen::MatrixXd>>("gain")) {
   this->set_gain(iso_gain);
 }
 
-template <>
-Linear<state_representation::CartesianState>::Linear(const state_representation::CartesianState& attractor, const std::vector<double>& diagonal_coefficients) : DynamicalSystem<state_representation::CartesianState>(state_representation::CartesianPose::Identity(attractor.get_reference_frame())),
-                                                                                                                                                                attractor_(std::make_shared<state_representation::Parameter<state_representation::CartesianState>>(state_representation::Parameter<state_representation::CartesianPose>("attractor", attractor))),
-                                                                                                                                                                gain_(std::make_shared<state_representation::Parameter<Eigen::MatrixXd>>("gain")) {
+template<>
+Linear<CartesianState>::Linear(const CartesianState& attractor, const std::vector<double>& diagonal_coefficients) :
+    DynamicalSystem<CartesianState>(attractor.get_reference_frame()),
+    attractor_(std::make_shared<Parameter<CartesianState>>(Parameter<CartesianPose>("attractor", attractor))),
+    gain_(std::make_shared<Parameter<Eigen::MatrixXd>>("gain")) {
   this->set_gain(diagonal_coefficients);
 }
 
-template <>
-Linear<state_representation::JointState>::Linear(const state_representation::JointState& attractor, const std::vector<double>& diagonal_coefficients) : DynamicalSystem<state_representation::JointState>(),
-                                                                                                                                                        attractor_(std::make_shared<state_representation::Parameter<state_representation::JointState>>(state_representation::Parameter<state_representation::JointPositions>("attractor", attractor))),
-                                                                                                                                                        gain_(std::make_shared<state_representation::Parameter<Eigen::MatrixXd>>("gain")) {
+template<>
+Linear<JointState>::Linear(const JointState& attractor, const std::vector<double>& diagonal_coefficients) :
+    DynamicalSystem<JointState>(),
+    attractor_(std::make_shared<Parameter<JointState>>(Parameter<JointPositions>("attractor", attractor))),
+    gain_(std::make_shared<Parameter<Eigen::MatrixXd>>("gain")) {
   this->set_gain(diagonal_coefficients);
 }
 
-template <>
-Linear<state_representation::CartesianState>::Linear(const state_representation::CartesianState& attractor, const Eigen::MatrixXd& gain_matrix) : DynamicalSystem<state_representation::CartesianState>(state_representation::CartesianPose::Identity(attractor.get_reference_frame())),
-                                                                                                                                                  attractor_(std::make_shared<state_representation::Parameter<state_representation::CartesianState>>(state_representation::Parameter<state_representation::CartesianPose>("attractor", attractor))),
-                                                                                                                                                  gain_(std::make_shared<state_representation::Parameter<Eigen::MatrixXd>>("gain")) {
+template<>
+Linear<CartesianState>::Linear(const CartesianState& attractor, const Eigen::MatrixXd& gain_matrix) :
+    DynamicalSystem<CartesianState>(attractor.get_reference_frame()),
+    attractor_(std::make_shared<Parameter<CartesianState>>(Parameter<CartesianPose>("attractor", attractor))),
+    gain_(std::make_shared<Parameter<Eigen::MatrixXd>>("gain")) {
   this->set_gain(gain_matrix);
 }
 
-template <>
-Linear<state_representation::JointState>::Linear(const state_representation::JointState& attractor, const Eigen::MatrixXd& gain_matrix) : DynamicalSystem<state_representation::JointState>(),
-                                                                                                                                          attractor_(std::make_shared<state_representation::Parameter<state_representation::JointState>>(state_representation::Parameter<state_representation::JointPositions>("attractor", attractor))),
-                                                                                                                                          gain_(std::make_shared<state_representation::Parameter<Eigen::MatrixXd>>("gain")) {
+template<>
+Linear<JointState>::Linear(const JointState& attractor, const Eigen::MatrixXd& gain_matrix)
+    : DynamicalSystem<JointState>(),
+      attractor_(std::make_shared<Parameter<JointState>>(Parameter<JointPositions>("attractor", attractor))),
+      gain_(std::make_shared<Parameter<Eigen::MatrixXd>>("gain")) {
   this->set_gain(gain_matrix);
 }
 
-template <>
-state_representation::CartesianState Linear<state_representation::CartesianState>::compute_dynamics(const state_representation::CartesianState& state) const {
-  state_representation::CartesianTwist twist = static_cast<const state_representation::CartesianPose&>(this->get_attractor()) - static_cast<const state_representation::CartesianPose&>(state);
+template<>
+CartesianState Linear<CartesianState>::compute_dynamics(const CartesianState& state) const {
+  CartesianTwist twist = CartesianPose(this->get_attractor()) - CartesianPose(state);
   twist *= this->get_gain();
   return twist;
 }
 
-template <>
-state_representation::JointState Linear<state_representation::JointState>::compute_dynamics(const state_representation::JointState& state) const {
-  state_representation::JointVelocities velocities = static_cast<const state_representation::JointPositions&>(this->get_attractor()) - static_cast<const state_representation::JointPositions&>(state);
+template<>
+JointState Linear<JointState>::compute_dynamics(const JointState& state) const {
+  JointVelocities velocities = JointPositions(this->get_attractor()) - JointPositions(state);
   velocities *= this->get_gain();
   return velocities;
 }

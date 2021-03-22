2410e6d4c1de4958801ae06c3e735565766c3122
Enrico Eberhard <enrico.eberhard@gmail.com>
2021-03-22 18:23:07 +0100

    Fix Build and Test action exit codes
    * Pipe output of make commands with || in case of failure
    to echo a relevant message and exit the script properly
    
    * Make printout lines clearer by using ">>> " message prefix
    
    Context: the CI was "passing" even when build or test stages
    failed, because the non-zero exit of the make commands
    was not handled properly.

--- .github/actions/build-test/entrypoint.sh
+++ .github/actions/build-test/entrypoint.sh
@@ -2,14 +2,15 @@
 
 cd /github/workspace/source && mkdir build && cd build || exit 1
 
-echo "Configuring cmake"
+echo ">>> Configuring cmake"
 cmake -DBUILD_CONTROLLERS=ON -DBUILD_DYNAMICAL_SYSTEMS=ON -DBUILD_ROBOT_MODEL=ON -DBUILD_TESTING=ON ..
 
-echo "Building project"
-make -j all
+echo ">>> Building project..."
+make -j all || (echo ">>> Build stage failed!" && exit 2) || exit $?
+echo ">>> Build stage completed successfully!"
 
-echo "Running all test stages"
-CTEST_OUTPUT_ON_FAILURE=1 make test
+echo ">>> Running all test stages..."
+CTEST_OUTPUT_ON_FAILURE=1 make test || (echo ">>> Test stage failed!" && exit 3) || exit $?
+echo ">>> Test stages completed successfully!"
 
-echo "Test stages completed successfully!"
 exit 0

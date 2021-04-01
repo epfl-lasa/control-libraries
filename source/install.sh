e328a02f6da494de6269be56dc5de5d70564e7b2
domire8 <71256590+domire8@users.noreply.github.com>
2021-04-01 14:26:27 +0200

    Parse arguments and provide help (#89)
    * feat: parse arguments and provide help in install.sh
    
    * feat: simpler arguments and no install of py bindings
    
    * fix: input argument parsing

--- source/install.sh
+++ source/install.sh
@@ -2,12 +2,36 @@
 SCRIPT=$(readlink -f "${BASH_SOURCE[0]}")
 SOURCE_PATH=$(dirname "$SCRIPT")
 
-# options
-# TODO: parse arguments and provide --help to set these options
-BUILD_TESTING="OFF"
 BUILD_CONTROLLERS="ON"
 BUILD_DYNAMICAL_SYSTEMS="ON"
 BUILD_ROBOT_MODEL="ON"
+BUILD_TESTING="OFF"
+
+FAIL_MESSAGE="The provided input arguments are not valid.
+Run the script with the '--help' argument."
+
+HELP_MESSAGE="Usage: ./install.sh [OPTIONS]
+
+An install script for the control libraries.
+
+Options:
+  --no-controllers         Exclude the controllers library
+  --no-dynamical-systems   Exclude the dynamical systems library
+  --no-robot-model         Exclude the robot model library
+  --build-tests            Build the unittest targets
+  --help                   Show this help message"
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --no-controllers) BUILD_CONTROLLERS="OFF"; shift 1;;
+    --no-dynamical-systems) BUILD_DYNAMICAL_SYSTEMS="OFF"; shift 1;;
+    --no-robot-model) BUILD_ROBOT_MODEL="OFF"; shift 1;;
+    --build-tests) BUILD_TESTING="ON"; shift 1;;
+    --help) echo "$HELP_MESSAGE"; exit 1;;
+
+    -*) echo "Unknown option: $1" >&2; echo "$FAIL_MESSAGE"; exit 1;;
+  esac
+done
 
 # install base dependencies
 apt-get update && apt-get install -y libeigen3-dev
@@ -20,7 +44,7 @@ if [ "${BUILD_ROBOT_MODEL}" == "ON" ]; then
   echo "deb [arch=amd64] http://robotpkg.openrobots.org/packages/debian/pub $(lsb_release -cs) robotpkg" | tee /etc/apt/sources.list.d/robotpkg.list
   curl http://robotpkg.openrobots.org/packages/debian/robotpkg.key | apt-key add -
 
-  apt-get update && apt-get install -y robotpkg-py38-pinocchio
+  apt-get update && apt-get install -y robotpkg-pinocchio
 
   export PATH=/opt/openrobots/bin:$PATH
   export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH

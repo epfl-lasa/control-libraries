b587dbc4c630c59d2df065bba1e75a61454535ae
patr3 <36916859+patr3@users.noreply.github.com>
2021-04-01 16:34:02 +0200

    Update source/robot_model/include/robot_model/exceptions/IKDoesNotConverge.hpp
    Co-authored-by: Enrico Eberhard <32450951+eeberhard@users.noreply.github.com>

--- source/robot_model/include/robot_model/exceptions/IKDoesNotConverge.hpp
+++ source/robot_model/include/robot_model/exceptions/IKDoesNotConverge.hpp
@@ -5,9 +5,13 @@
 namespace robot_model::exceptions {
 class IKDoesNotConverge : public std::exception {
 public:
-  IKDoesNotConverge(const int& iterations, const double& error) :
-  msg_("The inverse kinematics algorithm did not converge.\nThe residual error after " + std::to_string(iterations) + " iterations is " + std::to_string(error) + ".") { }
-  virtual char const *what() const noexcept { return msg_.c_str(); }
+  IKDoesNotConverge(int iterations, double error) :
+    msg_("The inverse kinematics algorithm did not converge.\nThe residual error after " + std::to_string(iterations) +
+         " iterations is " + std::to_string(error) + ".") {}
+
+   virtual char const *what() const noexcept {
+     return msg_.c_str();
+   }
 
 private:
   std::string msg_;

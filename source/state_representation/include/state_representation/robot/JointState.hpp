2f2f4e8b7195f6e69af6ae5771c823e9dc44fc94
Baptiste Busch <baptiste.busch@gmail.com>
2021-04-14 14:45:06 +0200

    fix: identation

--- source/state_representation/include/state_representation/robot/JointState.hpp
+++ source/state_representation/include/state_representation/robot/JointState.hpp
@@ -39,7 +39,8 @@ enum class JointStateVariable {
  * the distance on. Default ALL for full distance across all dimensions
  * @return the distance between the two states
  */
-double dist(const JointState& s1, const JointState& s2,
+double dist(const JointState& s1,
+            const JointState& s2,
             const JointStateVariable& state_variable_type = JointStateVariable::ALL);
 
 /**
@@ -282,7 +283,8 @@ public:
    * @param noise_ratio if provided, this value will be used to apply a dead zone under which
    * the velocity will be set to 0
    */
-  void clamp_state_variable(double max_absolute_value, const JointStateVariable& state_variable_type,
+  void clamp_state_variable(double max_absolute_value,
+                            const JointStateVariable& state_variable_type,
                             double noise_ratio = 0);
 
   /**
@@ -294,7 +296,8 @@ public:
    * the velocity will be set to 0
    */
   void clamp_state_variable(const Eigen::ArrayXd& max_absolute_value_array,
-                            const JointStateVariable& state_variable_type, const Eigen::ArrayXd& noise_ratio_array);
+                            const JointStateVariable& state_variable_type,
+                            const Eigen::ArrayXd& noise_ratio_array);
 
   /**
    * @brief Return a copy of the JointState
@@ -475,7 +478,7 @@ inline void JointState::set_state_variable(Eigen::VectorXd& state_variable, cons
   if (new_value.size() != this->get_size()) {
     throw IncompatibleSizeException(
         "Input vector is of incorrect size: expected " + std::to_string(this->get_size()) + ", given "
-        + std::to_string(new_value.size()));
+            + std::to_string(new_value.size()));
   }
   this->set_filled();
   state_variable = new_value;

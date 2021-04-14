aae2ab15176c24dbb263ec604de7f5de789f0261
Baptiste Busch <baptiste.busch@gmail.com>
2021-04-14 19:13:04 +0200

    fix: correct missing naming change

--- source/robot_model/include/robot_model/exceptions/InverseKinematicsNotConvergingException.hpp
+++ source/robot_model/include/robot_model/exceptions/InverseKinematicsNotConvergingException.hpp
@@ -6,7 +6,7 @@ namespace robot_model::exceptions {
 class InverseKinematicsNotConvergingException : public std::exception {
 public:
   InverseKinematicsNotConvergingException(unsigned int iterations, double error) :
-    msg_("The inverse geometry algorithm did not converge.\nThe residual error after " + std::to_string(iterations) +
+    msg_("The inverse kinematics algorithm did not converge.\nThe residual error after " + std::to_string(iterations) +
          " iterations is " + std::to_string(error) + ".") {}
 
    virtual char const *what() const noexcept {

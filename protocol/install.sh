2a8231d1fad671e337106c67eafb8d6ee9ad8879
Baptiste Busch <baptiste.busch@gmail.com>
2021-08-11 14:19:09 +0200

    feat: add install script for bindings

---
+++ protocol/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT=$(readlink -f "${BASH_SOURCE[0]}")
+PROTOBUF_DIR=$(dirname "${SCRIPT}")/protobuf
+CLPROTO_DIR=$(dirname "${SCRIPT}")/clproto_cpp
+
+INSTALL_DESTINATION="/usr/local"
+
+echo ">>> GENERATING PROTOBUF BINDINGS"
+cd ${PROTOBUF_DIR} && make -j all
+
+echo ">>> INSTALLING CLPROTO"
+cd ${CLPROTO_DIR} && mkdir -p build && cd build || exit 1
+
+cmake -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="${INSTALL_DESTINATION}" .. 
+
+make -j && make install

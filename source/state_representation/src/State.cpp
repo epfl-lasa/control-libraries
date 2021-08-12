a07b564d287cded23a2440709c753e6cfd974d43
Enrico Eberhard <32450951+eeberhard@users.noreply.github.com>
2021-08-12 10:57:49 +0200

    Acceleration message protocol bindings (#175)
    * Add JointAccelerations to protocol
    
    * Define new message type JointAccelerations
    and add it as a field to StateMessage
    
    * Add clproto binding function for
    JointAccelerations
    
    * Add test for JointAccelerations proto
    
    * Fix unused argument warning in State.cpp
    
    * Remove argument name invocation in function definition
    of State.cpp for unused variable
    
    * Add CartesianAcceleration message definition
    in proto in advance of class implementation
    
    * Reorder StateMessage field numbers in advance
    of first release

--- source/state_representation/src/State.cpp
+++ source/state_representation/src/State.cpp
@@ -12,15 +12,15 @@ State::State(const StateType& type, const std::string& name, const bool& empty)
 State::State(const State& state)
     : type_(state.type_), name_(state.name_), empty_(state.empty_), timestamp_(std::chrono::steady_clock::now()) {}
 
-void State::set_data(const Eigen::VectorXd& data) {
+void State::set_data(const Eigen::VectorXd&) {
   throw exceptions::NotImplementedException("set_data() is not implemented for the base State class");
 }
 
-void State::set_data(const std::vector<double>& data) {
+void State::set_data(const std::vector<double>&) {
   throw exceptions::NotImplementedException("set_data() is not implemented for the base State class");
 }
 
-void State::set_data(const Eigen::MatrixXd& data) {
+void State::set_data(const Eigen::MatrixXd&) {
   throw exceptions::NotImplementedException("set_data() is not implemented for the base State class");
 }
 

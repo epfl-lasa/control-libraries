930f1d5831e8fd1263ea1ca63ab8673d805829ca
Dominic Reber <dominic.reber@epfl.ch>
2021-10-05 14:05:47 +0200

    Add tests

--- python/source/robot_model/bind_model.cpp
+++ python/source/robot_model/bind_model.cpp
@@ -2,7 +2,6 @@
 
 #include <robot_model/Model.hpp>
 
-
 void model(py::module_& m) {
   m.def("create_urdf_from_string", &Model::create_urdf_from_string, "Creates a URDF file with desired path and name from a string (possibly the robot description string from the ROS parameter server).", "urdf_string"_a, "desired_path"_a);
 
@@ -22,8 +21,26 @@ void model(py::module_& m) {
   c.def("set_gravity_vector", &Model::set_gravity_vector, "Setter of the gravity vector.", "gravity"_a);
 //  c.def("get_pinocchio_model", &Model::get_pinocchio_model, "Getter of the pinocchio model.");
 
-  c.def("print_qp_problem", &Model::print_qp_problem, "Helper function to print the qp problem (for debugging).")
-  c.def("clamp_in_range", &Model::clamp_in_range, "Clamp the joint state variables (positions, velocities & torques) according to the limits provided by the model", "joint_state"_a);
+  c.def(
+      "compute_jacobian", py::overload_cast<const state_representation::JointPositions&, const std::string&>(&Model::compute_jacobian),
+      "Compute the Jacobian from a given joint state at the frame given in parameter.", "joint_positions"_a, "frame_name"_a = std::string(""));
+  c.def(
+      "compute_jacobian_time_derivative", py::overload_cast<const state_representation::JointPositions&, const state_representation::JointVelocities&, const std::string&>(&Model::compute_jacobian_time_derivative),
+      "Compute the time derivative of the Jacobian from given joint positions and velocities at the frame in parameter.", "joint_positions"_a, "joint_velocities"_a, "frame_name"_a = std::string(""));
+  c.def("compute_inertia_matrix", py::overload_cast<const state_representation::JointPositions&>(&Model::compute_inertia_matrix), "Compute the Inertia matrix from given joint positions.", "joint_positions"_a);
+  c.def(
+      "compute_inertia_torques", py::overload_cast<const state_representation::JointState&>(&Model::compute_inertia_torques),
+      "Compute the Inertia torques, i.e the inertia matrix multiplied by the joint accelerations. Joint positions are needed as well for computations of the inertia matrix.", "joint_state"_a);
+  c.def("compute_coriolis_matrix", py::overload_cast<const state_representation::JointState&>(&Model::compute_coriolis_matrix), "Compute the Coriolis matrix from a given joint state.", "joint_state"_a);
+  c.def(
+      "compute_coriolis_torques", py::overload_cast<const state_representation::JointState&>(&Model::compute_coriolis_torques),
+      "Compute the Coriolis torques, i.e. the Coriolis matrix multiplied by the joint velocities and express the result as a JointTorques.", "joint_state"_a);
+  c.def("compute_gravity_torques", py::overload_cast<const state_representation::JointPositions&>(&Model::compute_gravity_torques), "Compute the gravity torques.", "joint_positions"_a);
+
+  c.def("print_qp_problem", &Model::print_qp_problem, "Helper function to print the qp problem (for debugging).");
+//  c.def(
+//      "clamp_in_range", py::overload_cast<const state_representation::JointState&>(&Model::clamp_in_range),
+//      "Clamp the joint state variables (positions, velocities & torques) according to the limits provided by the model", "joint_state"_a);
 }
 
 void bind_model(py::module_& m) {
--- python/test/model/test_model.py
+++ python/test/model/test_model.py
@@ -1,8 +1,9 @@
-import unittest
 import os
+import unittest
 
 import numpy as np
-from robot_model import Model
+from robot_model import Model, create_urdf_from_string
+from state_representation import JointPositions, Jacobian
 
 ROBOT_MODEL_METHOD_EXPECTS = [
     'get_robot_name',
@@ -14,26 +15,109 @@ ROBOT_MODEL_METHOD_EXPECTS = [
     'get_base_frame',
     'get_gravity_vector',
     'set_gravity_vector',
+    'compute_jacobian',
+    'compute_jacobian_time_derivative',
+    'compute_inertia_matrix',
+    'compute_inertia_torques',
+    'compute_coriolis_matrix',
+    'compute_coriolis_torques',
+    'compute_gravity_torques',
     'print_qp_problem',
-    'clamp_in_range',
 ]
 
 
-class TestModel(unittest.TestCase):
+class TestRobotModel(unittest.TestCase):
+    robot_model = None
+    robot_name = None
+    urdf_path = None
+    create_urdf_test_path = None
+    joint_positions = None
+    tol = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.robot_name = "franka"
+        cls.urdf_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "panda_arm.urdf")
+        cls.robot_model = Model(cls.robot_name, cls.urdf_path)
+        cls.joint_positions = JointPositions(cls.robot_name, cls.robot_model.get_number_of_joints())
+        cls.create_urdf_test_path = "/tmp/urdf_test.urdf"
+        cls.tol = 1e-5
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove(cls.create_urdf_test_path)
+        except OSError as ex:
+            print(ex)
 
     def test_callable_methods(self):
         methods = [m for m in dir(Model) if callable(getattr(Model, m))]
         for expected in ROBOT_MODEL_METHOD_EXPECTS:
             self.assertIn(expected, methods)
 
-    def test_constructors(self):
-        robot = Model("test", os.path.join(os.path.dirname(os.path.realpath(__file__)), "panda_arm.urdf"))
-        print(robot.get_robot_name())
-        print(robot.get_urdf_path())
-        print(robot.get_number_of_joints())
-        print(robot.get_joint_frames())
-        print(robot.get_frames())
-        print(robot.get_base_frame())
+    def test_get_set_name(self):
+        self.assertEqual(self.robot_model.get_robot_name(), self.robot_name)
+        self.robot_model.set_robot_name("robot")
+        self.assertEqual(self.robot_model.get_robot_name(), "robot")
+
+    def test_get_urdf_path(self):
+        self.assertEqual(self.robot_model.get_urdf_path(), self.urdf_path)
+
+    def test_number_of_joints(self):
+        self.assertEqual(self.robot_model.get_number_of_joints(), 7)
+
+    def test_copy_constructor(self):
+        tmp = Model(self.robot_model)
+        self.assertEqual(tmp.get_robot_name(), self.robot_name)
+        self.assertEqual(tmp.get_urdf_path(), self.urdf_path)
+        self.assertEqual(tmp.get_number_of_joints(), 7)
+
+    def test_jacobian_names(self):
+        zero = JointPositions(self.robot_name, 7)
+        jac = self.robot_model.compute_jacobian(zero)
+        self.assertTrue(isinstance(jac, Jacobian))
+        [self.assertEqual("panda_joint" + str(i + 1), jac.get_joint_names()[i]) for i in range(7)]
+
+        self.assertEqual(jac.get_reference_frame(), "panda_link0")
+        self.assertEqual(jac.get_frame(), "panda_link8")
+
+        jac2 = self.robot_model.compute_jacobian(zero, "panda_link2")
+        self.assertEqual(jac2.get_reference_frame(), "panda_link0")
+        self.assertEqual(jac2.get_frame(), "panda_link2")
+
+    def test_jacobian_invalid(self):
+        dummy = JointPositions(self.robot_name, 6)
+        with self.assertRaises(ValueError):
+            self.robot_model.compute_jacobian(self.joint_positions, "panda_link99")
+            self.robot_model.compute_jacobian(dummy, "panda_link8")
+
+    def test_jacobian_size(self):
+        jac = self.robot_model.compute_jacobian(self.joint_positions, "panda_link2")
+        self.assertEqual(jac.cols(), self.joint_positions.get_size())
+        self.assertEqual(jac.rows(), 6)
+
+    def test_get_set_gravity(self):
+        self.assertTrue(np.linalg.norm(np.array([0, 0, -9.81]) - self.robot_model.get_gravity_vector()) < self.tol)
+        dummy_vector = np.random.rand(3)
+        self.assertFalse(np.linalg.norm(dummy_vector - self.robot_model.get_gravity_vector()) < self.tol)
+        self.robot_model.set_gravity_vector(dummy_vector)
+        self.assertTrue(np.linalg.norm(dummy_vector - self.robot_model.get_gravity_vector()) < self.tol)
+
+    def test_create_urdf_from_string(self):
+        with open(self.urdf_path, "r") as file:
+            content = file.read()
+            self.assertTrue(create_urdf_from_string(content, self.create_urdf_test_path))
+        model = Model("fromCreatedUrdf", self.create_urdf_test_path)
+        self.assertEqual(model.get_number_of_joints(), self.robot_model.get_number_of_joints())
+        self.assertEqual(len(model.get_frames()), len(self.robot_model.get_frames()))
+        [self.assertEqual(model.get_frames()[i], self.robot_model.get_frames()[i]) for i in
+         range(len(self.robot_model.get_frames()))]
+
+        self.assertFalse(create_urdf_from_string("dummy_string", "/dev/null/invalid.urdf"))
+        self.assertTrue(create_urdf_from_string("dummy_string", self.create_urdf_test_path))
+        with self.assertRaises(Exception):
+            Model("dummy", self.create_urdf_test_path)
+
 
 if __name__ == '__main__':
     unittest.main()

798ba9ac363eb2f2db776f1de6816d5d76903d7d
Dominic Reber <dominic.reber@epfl.ch>
2022-01-07 12:04:52 +0100

    Propagate DS refactor to demos

--- demos/ros_examples/scripts/task_space_control_loop.cpp
+++ demos/ros_examples/scripts/task_space_control_loop.cpp
@@ -4,7 +4,7 @@
 #include <tf/transform_broadcaster.h>
 #include <state_representation/space/cartesian/CartesianPose.hpp>
 #include <state_representation/space/cartesian/CartesianTwist.hpp>
-#include <dynamical_systems/Linear.hpp>
+#include "dynamical_systems/DynamicalSystemFactory.hpp"
 
 using namespace state_representation;
 using namespace dynamical_systems;
@@ -14,22 +14,22 @@ void send_transform(const CartesianPose& pose, const std::string& pose_name_pref
   static tf::TransformBroadcaster br;
   tf::Transform transform;
   transform.setOrigin(tf::Vector3(pose.get_position()(0), pose.get_position()(1), pose.get_position()(2)));
-  transform.setRotation(tf::Quaternion(pose.get_orientation().x(),
-                                        pose.get_orientation().y(),
-                                        pose.get_orientation().z(),
-                                        pose.get_orientation().w()));
+  transform.setRotation(
+      tf::Quaternion(
+          pose.get_orientation().x(), pose.get_orientation().y(), pose.get_orientation().z(),
+          pose.get_orientation().w()));
   std::string prefix = (pose_name_prefix != "") ? pose_name_prefix + "_" : "";
-  br.sendTransform(tf::StampedTransform(transform,
-                                        ros::Time::now(),
-                                        pose.get_reference_frame(),
-                                        prefix + pose.get_name()));
+  br.sendTransform(
+      tf::StampedTransform(
+          transform, ros::Time::now(), pose.get_reference_frame(), prefix + pose.get_name()));
 }
 
-CartesianPose control_loop_step(const CartesianPose& pose,
-                                const Linear<CartesianState>& ds,
-                                const std::chrono::nanoseconds& dt) {
+CartesianPose control_loop_step(
+    const CartesianPose& pose, const std::shared_ptr<IDynamicalSystem<CartesianState>>& ds,
+    const std::chrono::nanoseconds& dt
+) {
   // get the twist evaluated at current pose
-  CartesianTwist desired_twist = ds.evaluate(pose);
+  CartesianTwist desired_twist = ds->evaluate(pose);
   // integrate the twist and add it to the current pose
   CartesianPose new_pose = dt * desired_twist + pose;
   // return it
@@ -37,15 +37,19 @@ CartesianPose control_loop_step(const CartesianPose& pose,
 }
 
 void control_loop(const std::chrono::nanoseconds& dt, double tolerance) {
-  // set a desired target and a linear ds toward the target
+  // set a desired target and a point attractor ds toward the target
   CartesianPose target = CartesianPose::Random("frame");
-  Linear<CartesianState> linear_ds(target);
+  std::shared_ptr<IDynamicalSystem<CartesianState>>
+      ds = DynamicalSystemFactory<CartesianState>::create_dynamical_system(
+      DynamicalSystemFactory<CartesianState>::DYNAMICAL_SYSTEM::POINT_ATTRACTOR
+  );
+  ds->set_parameter(make_shared_parameter("attractor", target));
   // set a starting pose
   CartesianPose current_pose = CartesianPose::Random("frame");
   // loop until target is reached
   double distance;
   do {
-    current_pose = control_loop_step(current_pose, linear_ds, dt);
+    current_pose = control_loop_step(current_pose, ds, dt);
     distance = dist(current_pose, target, CartesianStateVariable::POSE);
     send_transform(target, "attractor");
     send_transform(current_pose);

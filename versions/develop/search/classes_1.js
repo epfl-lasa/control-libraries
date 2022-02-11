3f42b18eea5e1de9e633e1d6262eda93b3c1f7f8
domire8 <domire8@users.noreply.github.com>
2022-02-11 13:48:29 +0000

    deploy: aa4fedc25af4ea063bdc5dab23a8599c9c5ce134

--- versions/develop/search/classes_1.js
+++ versions/develop/search/classes_1.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['cartesianpose_0',['CartesianPose',['../classstate__representation_1_1_cartesian_pose.html',1,'state_representation']]],
-  ['cartesianstate_1',['CartesianState',['../classstate__representation_1_1_cartesian_state.html',1,'state_representation']]],
-  ['cartesiantwist_2',['CartesianTwist',['../classstate__representation_1_1_cartesian_twist.html',1,'state_representation']]],
-  ['cartesiantwistcontroller_3',['CartesianTwistController',['../classcontrollers_1_1impedance_1_1_cartesian_twist_controller.html',1,'controllers::impedance']]],
-  ['cartesianwrench_4',['CartesianWrench',['../classstate__representation_1_1_cartesian_wrench.html',1,'state_representation']]],
-  ['circular_5',['Circular',['../classdynamical__systems_1_1_circular.html',1,'dynamical_systems']]],
-  ['controller_6',['Controller',['../classcontrollers_1_1_controller.html',1,'controllers']]],
-  ['controller_3c_20state_5frepresentation_3a_3acartesianstate_20_3e_7',['Controller&lt; state_representation::CartesianState &gt;',['../classcontrollers_1_1_controller.html',1,'controllers']]]
+  ['cartesianacceleration_0',['CartesianAcceleration',['../classstate__representation_1_1_cartesian_acceleration.html',1,'state_representation']]],
+  ['cartesianpose_1',['CartesianPose',['../classstate__representation_1_1_cartesian_pose.html',1,'state_representation']]],
+  ['cartesianstate_2',['CartesianState',['../classstate__representation_1_1_cartesian_state.html',1,'state_representation']]],
+  ['cartesiantwist_3',['CartesianTwist',['../classstate__representation_1_1_cartesian_twist.html',1,'state_representation']]],
+  ['cartesiantwistcontroller_4',['CartesianTwistController',['../classcontrollers_1_1impedance_1_1_cartesian_twist_controller.html',1,'controllers::impedance']]],
+  ['cartesianwrench_5',['CartesianWrench',['../classstate__representation_1_1_cartesian_wrench.html',1,'state_representation']]],
+  ['circular_6',['Circular',['../classdynamical__systems_1_1_circular.html',1,'dynamical_systems']]],
+  ['controller_7',['Controller',['../classcontrollers_1_1_controller.html',1,'controllers']]],
+  ['controller_3c_20state_5frepresentation_3a_3acartesianstate_20_3e_8',['Controller&lt; state_representation::CartesianState &gt;',['../classcontrollers_1_1_controller.html',1,'controllers']]]
 ];

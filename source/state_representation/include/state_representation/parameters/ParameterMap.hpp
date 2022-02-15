aec10288d3ba95c1db28777d3fe8be8374499677
Enrico Eberhard <enrico.eberhard@gmail.com>
2022-02-15 15:32:32 +0100

    ParameterMap constructors and source
    * Add constructors for ParameterMap to set initial parameters
    
    * Add source file for ParameterMap implementations
    
    * Add InvalidParameterException

--- source/state_representation/include/state_representation/parameters/ParameterMap.hpp
+++ source/state_representation/include/state_representation/parameters/ParameterMap.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <list>
+#include <map>
+#include <memory>
+
 #include "state_representation/exceptions/InvalidParameterException.hpp"
 #include "state_representation/parameters/Parameter.hpp"
 
@@ -16,6 +20,20 @@ public:
    * @brief Empty constructor
    */
   ParameterMap() = default;
+
+  /**
+   * @brief Construct the parameter map with an initial list of parameters
+   * @param parameters A list of Parameter pointers
+   */
+  explicit ParameterMap(const std::list<std::shared_ptr<state_representation::ParameterInterface>>& parameters);
+
+  /**
+   * @brief Construct the parameter map with an initial map of parameters
+   * @param parameters A amp of Parameter pointers
+   */
+  explicit ParameterMap(
+      const std::map<std::string, std::shared_ptr<state_representation::ParameterInterface>>& parameters
+  );
 
   /**
    * @brief Get a parameter by its name.
@@ -94,70 +112,15 @@ protected:
 
 };
 
-inline std::shared_ptr<state_representation::ParameterInterface>
-ParameterMap::get_parameter(const std::string& name) const {
-  if (this->parameters_.find(name) == this->parameters_.cend()) {
-    throw exceptions::InvalidParameterException("Could not find a parameter named '" + name + "'.");
-  }
-  return this->parameters_.at(name);
-}
-
 template<typename T>
 inline T ParameterMap::get_parameter_value(const std::string& name) const {
   return std::static_pointer_cast<state_representation::Parameter<T>>(this->get_parameter(name))->get_value();
-}
-
-inline std::map<std::string, std::shared_ptr<state_representation::ParameterInterface>>
-ParameterMap::get_parameters() const {
-  return this->parameters_;
-}
-
-inline std::list<std::shared_ptr<state_representation::ParameterInterface>> ParameterMap::get_parameter_list() const {
-  std::list<std::shared_ptr<state_representation::ParameterInterface>> param_list;
-  for (const auto& param_it: this->parameters_) {
-    param_list.template emplace_back(param_it.second);
-  }
-  return param_list;
-}
-
-inline void ParameterMap::set_parameter(const std::shared_ptr<state_representation::ParameterInterface>& parameter) {
-  this->validate_and_set_parameter(parameter);
-}
-
-inline void ParameterMap::set_parameters(
-    const std::list<std::shared_ptr<state_representation::ParameterInterface>>& parameters
-) {
-  for (const auto& param: parameters) {
-    this->set_parameter(param);
-  }
-}
-
-inline void ParameterMap::set_parameters(
-    const std::map<std::string, std::shared_ptr<state_representation::ParameterInterface>>& parameters
-) {
-  for (const auto& param_it: parameters) {
-    this->set_parameter(param_it.second);
-  }
 }
 
 template<typename T>
 inline void ParameterMap::set_parameter_value(const std::string& name, const T& value) {
   using namespace state_representation;
   this->validate_and_set_parameter(std::make_shared<Parameter<T>>(Parameter<T>(name, value)));
-}
-
-inline void ParameterMap::assert_parameter_valid(
-    const std::shared_ptr<state_representation::ParameterInterface>& parameter
-) {
-  if (this->parameters_.at(parameter->get_name())->get_type() != parameter->get_type()) {
-    throw exceptions::InvalidParameterException(
-        "Parameter '" + parameter->get_name() + "' exists, but has unexpected type.");
-  }
-}
-
-inline void
-ParameterMap::validate_and_set_parameter(const std::shared_ptr<state_representation::ParameterInterface>& parameter) {
-  this->parameters_[parameter->get_name()] = parameter;
 }
 
 }

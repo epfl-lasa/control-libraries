effdf469ad926b4adc71a61210992b04b13ed78b
domire8 <domire8@users.noreply.github.com>
2022-03-25 10:53:58 +0000

    deploy: 7eaaceecbc41467d364d9b3af8d4095bb2e80cca

--- versions/develop/search/classes_9.js
+++ versions/develop/search/classes_9.js
@@ -8,6 +8,7 @@ var searchData=
   ['parametervalues_5',['ParameterValues',['../structpy__parameter_1_1_parameter_values.html',1,'py_parameter']]],
   ['pointattractor_6',['PointAttractor',['../classdynamical__systems_1_1_point_attractor.html',1,'dynamical_systems']]],
   ['predicate_7',['Predicate',['../classstate__representation_1_1_predicate.html',1,'state_representation']]],
-  ['pydynamicalsystem_8',['PyDynamicalSystem',['../class_py_dynamical_system.html',1,'']]],
-  ['pyparametermap_9',['PyParameterMap',['../class_py_parameter_map.html',1,'']]]
+  ['pycontroller_8',['PyController',['../class_py_controller.html',1,'']]],
+  ['pydynamicalsystem_9',['PyDynamicalSystem',['../class_py_dynamical_system.html',1,'']]],
+  ['pyparametermap_10',['PyParameterMap',['../class_py_parameter_map.html',1,'']]]
 ];

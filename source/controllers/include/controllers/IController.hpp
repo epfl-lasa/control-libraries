f05309b770358c879964960bc791d2bc73a94cba
Enrico Eberhard <enrico.eberhard@gmail.com>
2022-08-23 15:03:51 +0200

    Rename in Controllers

--- source/controllers/include/controllers/IController.hpp
+++ source/controllers/include/controllers/IController.hpp
@@ -20,7 +20,7 @@ namespace controllers {
 /**
  * @class IController
  * @brief Abstract class to define a controller in a desired state type, such as joint or Cartesian spaces
- * @tparam S the state type of the controller
+ * @tparam S The state type of the controller
  */
 template<class S>
 class IController : public state_representation::ParameterMap {
@@ -38,18 +38,18 @@ public:
   /**
    * @brief Compute the command output based on the commanded state and a feedback state
    * To be redefined based on the actual controller implementation.
-   * @param command_state the input state to the controller
-   * @param feedback_state the current state of the system given as feedback
-   * @return the output command at the input state
+   * @param command_state The input state to the controller
+   * @param feedback_state The current state of the system given as feedback
+   * @return The output command in the same state space as the input
    */
   [[nodiscard]] virtual S compute_command(const S& command_state, const S& feedback_state) = 0;
 
   /**
    * @brief Compute the command output in joint space
-   * @param command_state
-   * @param feedback_state
-   * @param jacobian the Jacobian matrix relating Cartesian forces to joint space
-   * @return
+   * @param command_state The input state to the controller
+   * @param feedback_state The current state of the system given as feedback
+   * @param jacobian The Jacobian matrix relating Cartesian forces to joint space
+   * @return The output command in joint space
    */
   [[nodiscard]] state_representation::JointState compute_command(
       const S& command_state, const S& feedback_state, const state_representation::Jacobian& jacobian
@@ -58,15 +58,15 @@ public:
   /**
    * @brief Compute the command output in joint space from command and feedback states in task space
    * @details This method requires the controller to have an associated robot model to compute the Jacobian.
-   * @param command_state
-   * @param feedback_state
+   * @param command_state The input state to the controller
+   * @param feedback_state The current state of the system given as feedback
    * @param joint_positions The current joint positions, which are required for computing the Jacobian
-   * @param frame_name
-   * @return
+   * @param frame The name of the robot frame at which to compute the Jacobian
+   * @return The output command in joint space
    */
   [[nodiscard]] state_representation::JointState compute_command(
       const S& command_state, const S& feedback_state, const state_representation::JointPositions& joint_positions,
-      const std::string& frame_name = ""
+      const std::string& frame = ""
   );
 
   /**
